4f7eaccf3603ead96c4f5e175082d75ae2b4ccd3
VIKKEE SINGH <vikkeesingh@gmail.com>
2017-10-02 01:50:37 +0530

    registration form and api

---
+++ client/src/components/Register.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1> Register </h1>
+    <input
+    type="email"
+    name="email"
+    v-model="email"
+    placeholder="email" />
+    <br><br>
+    <input
+    type="password"
+    name="password"
+    v-model="password"
+    placeholder="password" /><br>
+    <button @click="Register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: 'abc',
+      password: '123'
+    }
+  },
+  methods: {
+    async Register () {
+      await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+    }
+  }
+}
+</script>
+
+</style>
